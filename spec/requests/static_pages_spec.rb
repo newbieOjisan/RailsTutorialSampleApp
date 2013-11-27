82bdfbc5025e07ea51864c3d6ac33773f8c4d02b
newbieOjisan <newbie_ojisan@yahoo.co.jp>
2013-11-27 12:49:32 +0900

    chapter4 finished

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,16 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have the h1  'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1',:text=>'Sample App')
+      page.should have_selector('h1',:text => 'Sample App')
     end
     it "should have the title 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                 :text=> "#{base_title} | Home")
+                 :text => "#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',:text => '| Home')
     end
   end
 
