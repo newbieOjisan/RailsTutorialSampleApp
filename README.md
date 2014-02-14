06a5ccfb6a027dfd85ad912d905955725acdb06e
newbieOjisan <newbie_ojisan@yahoo.co.jp>
2014-02-14 14:01:41 +0900

    Add user following

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Ruby on Rails チュートリアル：サンプルアプリケーション
 
+Ruby on rails ver 3.2 版
+
 これは、以下のためのサンプルアプリケーションです。
 [*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.jp/)
 by [Michael Hartl](http://michaelhartl.com/).
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -68,6 +68,16 @@ describe "Authentication" do
         it { should have_selector('title',text: 'Sign in') }
       end
 
+      describe "visiting the following page" do
+        before { visit following_user_path(user) }
+        it { should have_selector('title',text: 'Sign in') }
+      end
+
+      describe "visiting the followers page" do
+        before { visit followers_user_path(user) }
+        it { should have_selector('title',text: 'Sign in') }
+      end
+
       describe "having no these links" do
         before { visit root_path }
         it { should_not have_link('Profile',href: user_path(user)) }
@@ -112,6 +122,18 @@ describe "Authentication" do
         end
         describe "submitting to the destroy action" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
           specify { response.should redirect_to(signin_path) }
         end
       end
